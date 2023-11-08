38d259da70aa96fbcc9f309248ad18dd49842869
En-naddam <abdoennaddam@gmail.com>
2023-11-08 19:35:25 +0100

    update & refactor database

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -4,6 +4,12 @@ const uri = process.env.MONGODB_URI;
 
 if (!uri) throw new Error("Add Mongo URI to .env.local");
 
+type TMongoose = { conn: typeof mongoose; promise: Promise<typeof mongoose> };
+
+declare module globalThis {
+  let mongoose: TMongoose;
+}
+
 let cached = globalThis.mongoose || { conn: null, promise: null };
 
 export const connect = async () => {
